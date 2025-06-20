bcd09cf4b9252ad4ffaa3e670acbd3b86da89c4e
mchu-1999 <jerry.ee06@nctu.edu.tw>
2025-06-20 04:52:20 +0200

    update navbars

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,9 +2,21 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   // "/",
-  "/comp-geom/",
-  "/geo-vis/",
-  "/coursework/",
+  {
+    text: "Computational-geometry",
+    icon: "folder-open-twotone",
+    link: "/comp-geom/",
+  },
+  {
+    text: "Geo-visualizations",
+    icon: "folder-open-twotone",
+    link: "/geo-vis/",
+  },
+  {
+    text: "Coursework",
+    icon: "folder-open-twotone",
+    link: "/coursework/",
+  },
   {
     text: "My CV",
     icon: "book",
